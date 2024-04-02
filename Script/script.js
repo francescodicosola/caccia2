bd64bf84c76caa4d938d7c699475cdcdc2249896
francescodicosola <162125454+francescodicosola@users.noreply.github.com>
2024-04-02 21:15:46 +0200

    Update script.js

--- Script/script.js
+++ Script/script.js
@@ -1,1 +1,7 @@
-
+function doLogin() {
+  var username = document.getElementById("Usarname") .value;
+  var psw = document.getElementById("Password") -value;
+  if(username == "" || psw == ""){
+     alert("Username e Password obbligatori");
+  ｝
+｝
