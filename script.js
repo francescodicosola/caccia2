c68df92be4b05156178a9f54afcf99efe1ead321
francescodicosola <162125454+francescodicosola@users.noreply.github.com>
2024-04-08 15:32:03 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-console.log('Hello!');
+ function doLogin(event) {
+  var username = document.getElementById("username").value;
+  var psw = document.getElementById("password").value;
+  if(username == "" || psw == ""){
+    alert("username e password sono obbligatori");
+  }
+  else {
+  fetch("https://www.cacciaapi.altervista.org/login.php/loginIpad", {
+   method: "POST", 
